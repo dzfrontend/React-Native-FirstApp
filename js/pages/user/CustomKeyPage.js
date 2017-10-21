8372f7a38d00dd1dfa71899270c8b6ddcb11634b
dengzhao <1301829682@qq.com>
2017-10-21 23:59:25 +0800

    自定义标签选择状态保存到AsyncStorage

--- js/pages/user/CustomKeyPage.js
+++ js/pages/user/CustomKeyPage.js
@@ -7,13 +7,16 @@ import{
 	StyleSheet,
 	Text,
 	TouchableOpacity,
-	ScrollView
+	ScrollView,
+	Image
 } from 'react-native'
 
 import NavigationBar from '../../common/NavigationBar'
 import NavigationBarUtils from '../../util/NavigationBarUtils'
 
-import LanguageDao,{ FLAG_LANGUAGE } from '../../expand/dao/LanguageDao'
+import LanguageDao,{ FLAG_LANGUAGE } from '../../expand/dao/LanguageDao' //类和变量
+import CheckBox from 'react-native-check-box' //第三方组件checkbox
+import ArrayUtils from '../../util/ArrayUtils' //array数组封装方法共用库
 
 export default class UserPage extends Component{
 	constructor(props){
@@ -21,12 +24,10 @@ export default class UserPage extends Component{
 		this.state = {
 			dataArray:[]
 		}
-		this.LanguageDao = new LanguageDao(FLAG_LANGUAGE.flag_key)
+		this.LanguageDao = new LanguageDao(FLAG_LANGUAGE.flag_key) //实例化class
+		this.changeValue = [] //记录用户自定义标签的修改
 	}
 
-	onSave(){
-		this.props.navigator.pop();
-	}
 
 	loadData(){
 		//取AsyncStorage
@@ -44,9 +45,80 @@ export default class UserPage extends Component{
 	componentDidMount() {
 		this.loadData()
 	}
-
+	
+	//自定义标签展示
 	renderView(){
-		return <Text>{JSON.stringify(this.state.dataArray)}</Text>
+		if(!this.state.dataArray || this.state.dataArray.length === 0){return}
+		let len = this.state.dataArray.length
+		let views = []
+		for(let i=0;i<len-2;i+=2){
+			views.push(
+				<View key={i}>
+					<View style={styles.item}>
+						{/*<Text>{this.state.dataArray[i].name}</Text>
+						<Text>{this.state.dataArray[i+1].name}</Text>*/}
+						{this.renderCheckBox(this.state.dataArray[i])}
+						{this.renderCheckBox(this.state.dataArray[i+1])}
+					</View>
+					<View style={styles.line}></View>
+				</View>
+			)
+		}
+		//遗漏元素，奇数遗漏1个元素，偶数遗漏2个元素
+		views.push(
+			<View key={len-1}>
+				<View style={styles.item}>
+					{len %2 === 0 ? this.renderCheckBox(this.state.dataArray[len-2]) : null}
+					{this.renderCheckBox(this.state.dataArray[len-1])}
+				</View>
+				<View style={styles.line}></View>
+			</View>
+		)
+		return views
+	}
+	//checkbox
+	checkBoxOnClick = (data) => {
+		data.checked = !data.checked
+		{
+		/*
+		//这里的代码移动到了公用util
+		for(var i=0,len=this.changeValue.length;i<len;i++){
+			//这里作用是把已经存在的元素去掉
+			var temp = this.changeValue[i]
+			if(temp === data){
+				this.changeValue.splice(i,1) //从数组的某个位置去掉一个元素
+				return
+			}
+		}
+		this.changeValue.push(data) //记录用户自定义标签的修改
+		*/
+		}
+		ArrayUtils.updateArray(this.changeValue,data)
+	}
+
+	onSave(){
+		if(this.changeValue.length === 0){
+			this.props.navigator.pop() //没变化
+			return
+		}
+		this.LanguageDao.save(this.state.dataArray) //记录保存到本地存储
+		this.props.navigator.pop();
+	}
+	renderCheckBox(data){
+		return(
+			<CheckBox
+				style={{flex:1,padding:10}}
+				leftText={data.name}
+				checkedImage={
+					<Image source={require('./img/ic_check_box.png')} style={{tintColor:'#6495ED'}}/>
+				}
+				unCheckedImage={
+					<Image source={require('./img/ic_check_box_outline_blank.png')}  style={{tintColor:'#6495ED'}}/>
+				}
+				isChecked={data.checked}
+				onClick={ () => this.checkBoxOnClick(data) }
+			/>
+		)
 	}
 	render(){
 		let rightButton = <TouchableOpacity
@@ -85,5 +157,13 @@ const styles = StyleSheet.create({
 	title:{
 		fontSize:20,
 		color:'#fff'
+	},
+	line:{
+		height:1,
+		backgroundColor:'darkgray'
+	},
+	item:{
+		flexDirection:'row',
+		alignItems:'center'
 	}
 })
